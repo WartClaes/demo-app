b79aa999c76c43f489de7ca80212773a9669f3bd
Wart Claes <wart@MacBook-Pro-van-Wart.local>
2024-07-21 11:12:02 +0200

    feat(backend): save responses in file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import crypto from 'crypto';
 
 import { getPhonemes, reverse } from './utils/string';
 
@@ -11,7 +13,6 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.post('/api', (req: Request, res: Response) => {
-  console.log(req.body);
   const {
     firstName,
     lastName,
@@ -20,13 +21,26 @@ app.post('/api', (req: Request, res: Response) => {
   const fullName = [firstName, lastName].join(' ');
   const { consonants, vowels } = getPhonemes(fullName);
 
-  res.json({
+  const responseData = {
     vowels,
     consonants,
     fullName,
     reversedFullname: reverse(fullName),
     requestData: req.body,
-  });
+  }
+
+  const hash = crypto.createHash('sha256');
+  hash.update(JSON.stringify(responseData));
+  const fileName = `${hash.digest('hex')}.txt`;
+
+  try {
+    fs.writeFileSync(fileName, JSON.stringify(responseData), 'utf8');
+    console.log('Data written to file successfully.');
+  } catch (err) {
+    console.error('An error occurred while writing to the file:', err);
+  }
+
+  res.json(responseData);
 });
 
 app.listen(port, () => {
